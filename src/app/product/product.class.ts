870a8f98d7fc47d987b127ab229787519c198ba4
Hayden Brown <haydenbrowncinci@gmail.com>
2022-04-07 22:42:50 -0400

    added product module

---
+++ src/app/product/product.class.ts
@@ -0,0 +1,15 @@
+import { Vendor } from "../vendor/vendor.class";
+
+export class Product {
+
+    id: number = 0;
+    partNbr: string = "";
+    name: string = "";
+    price: number = 0;
+    unit: number = 0;
+    photoPath: string = "";
+
+    vendorId: number = 0;
+    vendor!: Vendor;
+
+}
